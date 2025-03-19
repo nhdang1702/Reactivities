48efb617602fce0cca60b7c47e8744588278b1b3
nhdang1702 <danghn1702@gmail.com>
2025-03-19 11:58:34 +0700

    add mobx

--- client/src/lib/api/agent.ts
+++ client/src/lib/api/agent.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import { store } from "../stores/store";
 
 const sleep = (delay: number) => {
     return new Promise(resolve => {   
@@ -9,14 +10,20 @@ const agent = axios.create({
     baseURL: import.meta.env.VITE_API_URL
 });
 
-agent.interceptors.request.use(async response => {
+agent.interceptors.request.use(config => {
+    store.uiStore.isBusy();
+    return config;
+})
+
+agent.interceptors.response.use(async response => {
     try {
         await sleep(1000);
         return response;
-    }
-    catch (error) {
+    } catch (error) {
         console.error(error);
         return Promise.reject(error);
+    } finally {
+        store.uiStore.isIdle();
     }
 });
 
